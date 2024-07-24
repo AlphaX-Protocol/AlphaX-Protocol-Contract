3e35b4b32154766b1106e444c45724812edff654
liberhex <holiberhex@gmail.com>
2024-07-24 16:29:25 +0800

    add erc20 permit

--- contracts/mock/DEXVaultV1.sol
+++ contracts/mock/DEXVaultV1.sol
@@ -3,4 +3,32 @@ pragma solidity ^0.8.24;
 
 import "../DEXVault.sol";
 
-contract DEXVaultV1 is DEXVault {}
+contract DEXVaultV1 is DEXVault {
+
+ using SafeERC20 for IERC20;
+      function depositWithPermit(
+        address token,
+        uint256 amount,
+        address receiver,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public tokenWhitelist(address(token)) whenNotPaused nonReentrant {
+        require(amount > 0, "Deposit amount must be greater than zero");
+
+        IERC20Permit(token).permit(
+            msg.sender,
+            address(this),
+            amount,
+            deadline,
+            v,
+            r,
+            s
+        );
+
+        IERC20(token).safeTransferFrom(msg.sender, address(this), amount);
+
+        emit Deposit(msg.sender, receiver, token, amount);
+    }  
+}
